79852ecb9b25b9007119c954121f6d2fa4caec46
yanwensheng <582826846@qq.com>
2019-06-03 16:59:40 +0800

    add base-stock-impl

--- base-stock/src/main/java/com/zhwl/controller/book/BookController.java
+++ base-stock/src/main/java/com/zhwl/controller/book/BookController.java
@@ -7,6 +7,7 @@ import com.zhwl.feign.BookServiceFeign;
 import com.zhwl.result.ResultVo;
 import com.zhwl.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,19 +15,22 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     @Autowired
-    private BookServiceFeign bookServiceFeign;
+    //private BookServiceFeign bookServiceFeign;
+    private BookService bookService;
 
     //新增
     @PostMapping
     public ResultVo add(@RequestBody Book book){
-        return ResultVo.ok(bookServiceFeign.add(book));
+        //return ResultVo.ok(bookServiceFeign.add(book));
+        return ResultVo.ok(bookService.add(book));
     }
 
     //查询
     @GetMapping("getAll")
     public ResultVo getAll(){
         try {
-            return ResultVo.ok(bookServiceFeign.getAll());
+            //return ResultVo.ok(bookServiceFeign.getAll());
+            return ResultVo.ok(bookService.getAll());
         }catch (BaseException e){
             e.printStackTrace();
             return ResultVo.fail(e.getMessage());
